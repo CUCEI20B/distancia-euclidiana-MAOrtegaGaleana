a9c5bf592565cda027cfd79d913d00a6c2d42852
Miguel Ortega <miguel.ortega5807@alumnos.udg.mx>
2020-10-19 09:36:02 -0500

    Cambio a la función distancia euclidiana

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,5 @@
 import math
 
 def distancia_euclidiana(origenx,destinox,origeny,destinoy):
-    s = math.sqrt((destinox-origenx)**2+(destinoy-origeny)**2)
-    return(s)
+    return(math.sqrt((destinox-origenx)**2+(destinoy-origeny)**2))
     
